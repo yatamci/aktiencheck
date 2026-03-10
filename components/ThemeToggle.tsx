2b5e61e20a6734cac556c75873d8c9174bcff48e
yatamci <yatamci@yahoo.com>
2026-03-10 20:08:47 +0100

    Update ThemeToggle.tsx

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -2,20 +2,19 @@
 
 export default function ThemeToggle(){
 
-const toggle = ()=>{
-
+function toggle(){
 document.documentElement.classList.toggle("dark")
-
 }
 
 return(
 
 <button
 onClick={toggle}
-className="border px-3 py-1 rounded"
+className="border rounded px-3 py-1 text-lg"
 >
 🌙
 </button>
 
 )
+
 }
