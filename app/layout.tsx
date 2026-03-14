2e2a36001d09c697158cfc2d113d2fd276739648
yatamci <yatamci@yahoo.com>
2026-03-14 14:59:24 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Aktiencheck',
+  icons: { icon: '/favicon.svg' },
   description: 'Analysiere Aktien mit wichtigen Finanzkennzahlen auf einen Blick.',
 }
 
@@ -10,6 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="de" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📈</text></svg>" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
